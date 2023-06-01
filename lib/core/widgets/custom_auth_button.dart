871c55b51e76e16a4c2658338e6603015b997098
mahmoud <mahmoudrhamza20@gmail.com>
2023-06-01 11:56:02 +0300

    add firebase

--- lib/core/widgets/custom_auth_button.dart
+++ lib/core/widgets/custom_auth_button.dart
@@ -1,7 +1,6 @@
 import 'package:e_commerce/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../utils/app_text_style.dart';
 
 Widget customAuthButton({required String text, required VoidCallback onTap}) {
